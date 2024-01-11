58d685ea74b9c2a799d69db0e29d11c231f26301
ChuckGarcian <chuckgarcian@gmail.com>
2024-01-11 16:02:42 -0600

    add new raylib project

---
+++ _build/obj/x64/Debug/llm-agent-survival/screen_ending.d
@@ -0,0 +1,10 @@
+obj/x64/Debug/llm-agent-survival/screen_ending.o: \
+ ../game/src/screen_ending.c /usr/include/stdc-predef.h \
+ ../raylib-master/src/raylib.h \
+ /usr/lib/gcc/x86_64-linux-gnu/11/include/stdarg.h \
+ /usr/lib/gcc/x86_64-linux-gnu/11/include/stdbool.h ../game/src/screens.h
+/usr/include/stdc-predef.h:
+../raylib-master/src/raylib.h:
+/usr/lib/gcc/x86_64-linux-gnu/11/include/stdarg.h:
+/usr/lib/gcc/x86_64-linux-gnu/11/include/stdbool.h:
+../game/src/screens.h:
---
+++ _build/obj/x64/Debug/llm-agent-survival/screen_title.d
@@ -0,0 +1,10 @@
+obj/x64/Debug/llm-agent-survival/screen_title.o: \
+ ../game/src/screen_title.c /usr/include/stdc-predef.h \
+ ../raylib-master/src/raylib.h \
+ /usr/lib/gcc/x86_64-linux-gnu/11/include/stdarg.h \
+ /usr/lib/gcc/x86_64-linux-gnu/11/include/stdbool.h ../game/src/screens.h
+/usr/include/stdc-predef.h:
+../raylib-master/src/raylib.h:
+/usr/lib/gcc/x86_64-linux-gnu/11/include/stdarg.h:
+/usr/lib/gcc/x86_64-linux-gnu/11/include/stdbool.h:
+../game/src/screens.h:
